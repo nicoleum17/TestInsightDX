c2f3adfc4f0782fc66e717730f37e4f1760d1f96
Frida Arcadia <frida.arcadialv@gmail.com>
2025-04-19 18:15:42 -0600

    Fix(graphDisplay): fix the conditional to display each graph

--- controllers/psicologa.controllers.js
+++ controllers/psicologa.controllers.js
@@ -158,11 +158,10 @@ exports.get_buscar = (request, response, next) => {
 exports.get_respuestasA = (request, response, next) => {
   const idUsuario = request.params.idusuario;
   const idPrueba = request.params.idprueba;
-
-    Aspirante.fetchOne(idUsuario).then(([datosAspirante, fieldData]) => {
-      PerteneceGrupo.fetchOne(idUsuario).then(([rows, fieldData]) => {
-        Grupo.fetchOneId(rows[0].idGrupo).then(([grupoRows, fieldData]) => {
-          if (idPrueba == 1) {
+  Aspirante.fetchOne(idUsuario).then(([datosAspirante, fieldData]) => {
+    PerteneceGrupo.fetchOne(idUsuario).then(([rows, fieldData]) => {
+      Grupo.fetchOneId(rows[0].idGrupo).then(([grupoRows, fieldData]) => {
+        if (idPrueba == 1) {
           ResultadosKostick.fetchAll(rows[0].idGrupo, idUsuario).then(
             (resultados) => {
               response.render("consultaRespuestasAspirante", {
@@ -193,9 +192,9 @@ exports.get_respuestasA = (request, response, next) => {
             }
           );
         }
-        });
       });
     });
+  });
 };
 
 exports.get_respuestasG = (request, response, next) => {
